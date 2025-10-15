ee7644186c2324e1b40960dac6561d7df61da68c
Érick Ferreira Corrêa <ericktubarao2018@gmail.com>
2025-10-15 17:48:48 -0300

    Add support for ES module require in server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
